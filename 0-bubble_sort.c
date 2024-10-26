b52e1761d83000909f829d9c3f7ea9d1666e5f68
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-10-25 18:44:40 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp, swapped;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,12 +23,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
-			break;
-
 	}
 }
