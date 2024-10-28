eb1c5cef786b3c9716fbad02760a5e5942ede891
laila <laila.mf2005@gmail.com>
2024-10-28 03:34:20 -0700

    task1

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 #endif
