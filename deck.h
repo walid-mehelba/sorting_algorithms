a30f827f8cd77ca7c38a1c9210eee5c8461ed29e
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-10-29 20:11:10 -0700

    task-12

--- deck.h
+++ deck.h
@@ -1,13 +1,15 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdlib.h>
+
 /**
-* enum kind_e - card kind
-* @SPADE: spade kind
-* @HEART: heart kind
-* @CLUB: club kind
-* @DIAMOND: diamond kind
-*/
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
 	SPADE = 0,
@@ -17,12 +19,12 @@ typedef enum kind_e
 } kind_t;
 
 /**
-* struct card_s - Playing card
-*
-* @value: Value of the card
-* From "Ace" to "King"
-* @kind: Kind of the card
-*/
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
 typedef struct card_s
 {
 	const char *value;
@@ -30,12 +32,12 @@ typedef struct card_s
 } card_t;
 
 /**
-* struct deck_node_s - Deck of card
-*
-* @card: Pointer to the card of the node
-* @prev: Pointer to the previous node of the list
-* @next: Pointer to the next node of the list
-*/
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
 typedef struct deck_node_s
 {
 	const card_t *card;
@@ -44,4 +46,5 @@ typedef struct deck_node_s
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
-#endif
+
+#endif /* DECK_H */
